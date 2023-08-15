183e09ab12ceeb3ec93b1eba39ed8803b2d5aca2
wcampbell <wcampbell1995@gmail.com>
2023-08-15 19:32:10 -0400

    Update to working MVP

--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,132 @@
+use std::ffi::CString;
+
 use deku::prelude::*;
 
+pub const VTBL_VOLID: u32 = 0x7fffefff;
 pub const UBI_EC_HDR_MAGIC: &[u8] = b"UBI#";
 pub const UBI_VID_HDR_MAGIC: &[u8] = b"UBI!";
 
 /// Erase Counter Header
 ///
 /// See <kernel>/drivers/mtd/ubi/ubi-media.h
-#[derive(Debug, DekuRead, DekuWrite)]
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Clone, Hash)]
 #[deku(endian = "big")]
 pub struct EcHdr {
+    /// magic string `UBI#`
     #[deku(assert_eq = "UBI_EC_HDR_MAGIC")]
     pub magic: [u8; 4],
+    /// UBI version meant to accept this image
     pub version: u8,
+    /// Reserved for future use, assert zeros
     pub padding1: [u8; 3],
+    /// Erase counter
     pub ec: u64,
+    /// Where the VID header starts
     pub vid_hdr_offset: u32,
+    /// Where user data starts
     pub data_offset: u32,
+    /// Image sequence number
     pub image_seq: u32,
+    /// Reserved for future use, assert zeros
     pub padding2: [u8; 32],
+    /// EC header crc32 checksum
     pub hdr_crc: u32,
 }
 
 /// Volume Identifier Header
 /// See <kernel>/drivers/mtd/ubi/ubi-media.h
-#[derive(Debug, DekuRead, DekuWrite)]
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq)]
 #[deku(endian = "big")]
 pub struct VidHdr {
+    /// Magic String `UBI!`
     #[deku(assert_eq = "UBI_VID_HDR_MAGIC")]
     pub magic: [u8; 4],
+    /// UBI version meant to accept this image
     #[deku(assert_eq = "1")]
     pub version: u8,
-    pub vol_type: u8,
+    pub vol_type: VolType,
+    /// If this is a copied PEB b/c of wear leveling
     pub copy_flag: u8,
+    /// Compatibility of this volume
     pub compat: u8,
+    /// ID of this volume
     pub vol_id: u32,
+    /// LEB number
     pub lnum: u32,
+    /// Reversed for future use, assert zeros
     pub padding1: [u8; 4],
+    /// How many bytes of data this contains (static types only)
     pub data_size: u32,
+    /// Total number of used LEBs in this volume
     pub used_ebs: u32,
+    /// How many bytes at the end of LEB are not used
     pub data_pad: u32,
+    /// CRC32 checksum of data, static type only
     pub data_crc: u32,
+    /// Reserved for future use, assert zeros
     pub padding2: [u8; 4],
+    /// Sequence number
     pub sqnum: u64,
+    /// Reserved for future use, assert zeros
     pub padding3: [u8; 12],
+    /// VID header CRC32 checksum
     pub hdr_crc: u32,
 }
 
 const UBI_VOL_NAME_MAX: usize = 127;
 
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Hash)]
+#[deku(ctx = "endian: deku::ctx::Endian", endian = "endian")]
+#[deku(type = "u8")]
+pub enum VolType {
+    /// Volume can be resized
+    Dynamic = 1,
+    /// Volume can not be resized
+    Static = 2,
+}
+
 /// See <kernel>/drivers/mtd/ubi/ubi-media.h
-#[derive(Debug, DekuRead, DekuWrite)]
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Hash)]
 #[deku(endian = "big")]
 pub struct VtblRecord {
+    /// How many PEBs reserved for this volume
     pub reserved_pebs: u32,
+    /// Volume alignment
     pub alignment: u32,
+    /// Number of unused bytes at end of PEB
     pub data_pad: u32,
-    pub vol_type: u8,
+    pub vol_type: VolType,
+    /// Vol update started but not finished
     pub upd_marker: u8,
+    #[deku(assert = "*name_len < UBI_VOL_NAME_MAX as u16")]
     pub name_len: u16,
     pub name: [u8; UBI_VOL_NAME_MAX + 1],
+    /// Volume flags
     pub flags: u8,
+    /// Reserved for future use, assert zeros
     pub padding: [u8; 23],
+    /// Vol record crc32 checksum
     pub crc: u32,
+}
+
+impl VtblRecord {
+    pub const SIZE: usize = 4 * 4 + UBI_VOL_NAME_MAX + 1 + 24 + 4;
+
+    pub fn name(&self) -> Option<CString> {
+        if self.name_len == 0 {
+            None
+        } else {
+            let val = CString::from_vec_with_nul(self.name[..self.name_len as usize + 1].to_vec())
+                .unwrap();
+            Some(val)
+        }
+    }
 }
 
 const UBI_FM_MAX_BLOCKS: usize = 32;
 
 /// See <kernel>/drivers/mtd/ubi/ubi-media.h
-#[derive(DekuRead, DekuWrite)]
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Hash)]
 #[deku(endian = "big")]
 pub struct FmSb {
     pub magic: u32,
@@ -84,7 +143,7 @@ pub struct FmSb {
 const UBI_FM_MAX_POOL_SIZE: usize = 256;
 
 /// See <kernel>/drivers/mtd/ubi/ubi-media.h
-#[derive(DekuRead, DekuWrite)]
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Hash)]
 #[deku(endian = "big")]
 pub struct FmScanPool {
     pub magic: u32,
@@ -93,14 +152,14 @@ pub struct FmScanPool {
     pub padding: [u32; 4],
 }
 
-#[derive(DekuRead, DekuWrite)]
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Hash)]
 #[deku(endian = "big")]
 pub struct FmEc {
     pub magic: u32,
     pub ec: u32,
 }
 
-#[derive(DekuRead, DekuWrite)]
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Hash)]
 #[deku(endian = "big")]
 pub struct FmVolHdr {
     pub magic: u32,
@@ -112,7 +171,7 @@ pub struct FmVolHdr {
     pub padding2: [u8; 8],
 }
 
-#[derive(DekuRead, DekuWrite)]
+#[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Hash)]
 #[deku(endian = "big")]
 pub struct FmEba {
     pub magic: u32,
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,158 @@
-#![feature(cursor_remaining)]
+#![feature(int_roundings)]
 
 use std::collections::HashMap;
-use std::ffi::{CStr, CString};
-use std::io::{Seek, SeekFrom, Write};
 
-use deku::DekuContainerRead;
-use income::{EcHdr, VidHdr, VtblRecord};
+use std::env;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom, Write};
+
+use deku::prelude::*;
+use income::{EcHdr, VidHdr, VolType, VtblRecord, UBI_EC_HDR_MAGIC, VTBL_VOLID};
 
 pub fn main() {
-    let block_size = 0x2_0000;
+    let args: Vec<String> = env::args().collect();
 
-    let mut volume_name = vec![];
-    let mut volumes = HashMap::new();
+    // read UBI Block type image starting with `UBI#` (EcHdr)
+    let mut file = std::fs::File::options().read(true).open(&args[1]).unwrap();
+    let file_len = file.metadata().unwrap().len();
 
-    let fs_bytes = std::fs::read("1294.ubi").unwrap();
-    let mut start = 0;
-
-    for _ in 0..(fs_bytes.len() / block_size) {
-        let mut bytes = std::io::Cursor::new(&fs_bytes[start..][..block_size]);
-        let (_, ec_header) = EcHdr::from_bytes((bytes.get_ref(), 0)).unwrap();
-
-        bytes
-            .seek(SeekFrom::Start(ec_header.vid_hdr_offset.into()))
-            .unwrap();
-        let (_, vid_header) = VidHdr::from_bytes((bytes.remaining_slice(), 0)).unwrap();
-
-        if vid_header.vol_id == 0x7fffefff {
-            bytes
-                .seek(SeekFrom::Start(ec_header.data_offset.into()))
-                .unwrap();
-            let mut rest = bytes.remaining_slice();
-            for (i, _) in (0..128).enumerate() {
-                let (l_rest, v_record) = VtblRecord::from_bytes((rest, 0)).unwrap();
-                let name = v_record.name[..v_record.name_len as usize + 1].to_vec();
-                volume_name.push(CString::from_vec_with_nul(name).unwrap());
-                rest = l_rest.0;
-                if v_record.crc == 0xf116c36b {
-                    break;
-                }
-                println!("{v_record:02x?}");
-            }
-        } else {
-            // TODO: data size is only used for crc?
-            //if vid_header.data_size != 0 {
-            //    let data = &bytes.remaining_slice()[0x800..][..vid_header.data_size as usize];
-            //    println!("BYTES: {:02x?}", data.len());
-            //    let bytes = volumes.entry(vid_header.vol_id).or_insert(vec![]);
-            //    bytes.write_all(data);
-            //} else {
-            let data = &bytes.remaining_slice()[0x800..];
-            let bytes = volumes.entry(vid_header.vol_id).or_insert(vec![]);
-            bytes.write_all(data).unwrap();
-            //}
+    let mut block_size = 0; // or lebsize
+                            // find block size
+    for n in 10..20 {
+        file.seek(SeekFrom::Start(1 << n)).unwrap();
+        let mut buf = [0; 4];
+        file.read_exact(&mut buf).unwrap();
+        if buf == UBI_EC_HDR_MAGIC {
+            block_size = 1 << n;
+            break;
         }
-        start += block_size;
     }
 
-    for (i, (_, volume)) in volumes.iter().enumerate() {
-        let name = volume_name[i].clone().into_string().unwrap();
-        println!("wrote: {name}");
-        std::fs::write(name, volume).unwrap();
+    let maxlebs = file_len.div_floor(block_size);
+
+    // scanblocks()
+    let mut map = HashMap::<u32, Vec<u64>>::new();
+    for lnum in 0..maxlebs {
+        // Ec
+        file.seek(SeekFrom::Start(lnum * block_size + 0)).unwrap();
+        let mut container = Container::new(file.try_clone().unwrap());
+        let ec = EcHdr::from_reader(&mut container, ()).unwrap();
+
+        // Vid
+        file.seek(SeekFrom::Start(
+            lnum * block_size + u64::from(ec.vid_hdr_offset),
+        ))
+        .unwrap();
+        let mut container = Container::new(file.try_clone().unwrap());
+        let vid = VidHdr::from_reader(&mut container, ()).unwrap();
+
+        if let Some(x) = map.get_mut(&vid.vol_id) {
+            x.push(lnum);
+        } else {
+            map.insert(vid.vol_id, vec![lnum]);
+        }
+    }
+
+    // read the volume table
+    let lnum = map[&VTBL_VOLID][0]; // TODO: i guess the first entry?
+    let start_of_volume = lnum * block_size;
+    file.seek(SeekFrom::Start(start_of_volume)).unwrap();
+    let mut container = Container::new(file.try_clone().unwrap());
+    let ec = EcHdr::from_reader(&mut container, ()).unwrap();
+
+    file.seek(SeekFrom::Start(
+        lnum * block_size + u64::from(ec.vid_hdr_offset),
+    ))
+    .unwrap();
+    let mut container = Container::new(file.try_clone().unwrap());
+    let vid = VidHdr::from_reader(&mut container, ()).unwrap();
+    assert_eq!(vid.lnum as u64, lnum);
+
+    let mut vtable = vec![];
+    file.seek(SeekFrom::Start(start_of_volume + ec.data_offset as u64))
+        .unwrap();
+
+    // TODO: I need to figure this out...
+    //
+    // You'll see this. with data_offset at 0x80, we still need to read something(???) until 0x12c
+    // where a VtblRecord starts.
+    //
+    //   10   │ │00000080│ 00 00 00 00 00 00 00 00 ┊ 00 00 00 00 00 00 00 00 │⋄⋄⋄⋄⋄⋄⋄⋄┊⋄⋄⋄⋄⋄⋄⋄⋄│
+    //   11   │ │*       │                         ┊                         │        ┊        │
+    //   12   │ │00000120│ 00 00 00 00 00 00 00 00 ┊ f1 16 c3 6b 00 00 00 01 │⋄⋄⋄⋄⋄⋄⋄⋄┊×•×k⋄⋄⋄•│
+    //   13   │ │00000130│ 00 00 00 01 00 00 00 00 ┊ 02 00 00 05 61 70 70 6c │⋄⋄⋄•⋄⋄⋄⋄┊•⋄⋄•appl│
+    //   14   │ │00000140│ 65 00 00 00 00 00 00 00 ┊ 00 00 00 00 00 00 00 00 │e⋄⋄⋄⋄⋄⋄⋄┊⋄⋄⋄⋄⋄⋄⋄⋄│
+    //   15   │ │00000150│ 00 00 00 00 00 00 00 00 ┊ 00 00 00 00 00 00 00 00 │⋄⋄⋄⋄⋄⋄⋄⋄┊⋄⋄⋄⋄⋄⋄⋄⋄│
+
+    let end_of_volume = start_of_volume + block_size;
+    let mut n = 0;
+    while file.stream_position().unwrap() < end_of_volume - VtblRecord::SIZE as u64 {
+        let mut container = Container::new(file.try_clone().unwrap());
+        let save_before_position = file.stream_position().unwrap();
+        match VtblRecord::from_reader(&mut container, ()) {
+            Ok(vid) => {
+                vtable.push((n, vid));
+            }
+            Err(_) => {
+                // rewind
+                file.seek(SeekFrom::Start(
+                    save_before_position + VtblRecord::SIZE as u64,
+                ))
+                .unwrap();
+            }
+        }
+        n += 1;
+    }
+
+    // UbiBlocks
+    println!(
+        "{} named volumes, {} physical volumes, blocksize={block_size:#x?}",
+        vtable.len(),
+        map.len()
+    );
+
+    for (volume, v) in vtable {
+        println!("Extracting volume: {:?}", v.name().unwrap());
+
+        let extract_map = &map[&volume];
+        let mut file_write = File::options()
+            .write(true)
+            .create(true)
+            .open(v.name().unwrap().to_str().unwrap())
+            .unwrap();
+
+        for lnum in extract_map {
+            let seek = SeekFrom::Start(lnum * block_size);
+            // TODO: we already read the ec, cache it
+            file.seek(seek).unwrap();
+            let mut container = Container::new(file.try_clone().unwrap());
+            let ec = EcHdr::from_reader(&mut container, ()).unwrap();
+
+            file.seek(SeekFrom::Start(
+                lnum * block_size + u64::from(ec.vid_hdr_offset),
+            ))
+            .unwrap();
+            let mut container = Container::new(file.try_clone().unwrap());
+            let vid = VidHdr::from_reader(&mut container, ()).unwrap();
+
+            match vid.vol_type {
+                VolType::Static => {
+                    file.seek(SeekFrom::Start(lnum * block_size + ec.data_offset as u64))
+                        .unwrap();
+                    let mut buf = vec![0; vid.data_size as usize];
+                    // change to io::copy
+                    file.read_exact(&mut buf).unwrap();
+                    file_write.write(&buf).unwrap();
+                }
+                VolType::Dynamic => {
+                    file.seek(SeekFrom::Start(lnum * block_size + ec.data_offset as u64))
+                        .unwrap();
+                    let mut buf = vec![0; block_size as usize - ec.data_offset as usize];
+                    // change to io::copy
+                    file.read_exact(&mut buf).unwrap();
+                    file_write.write(&buf).unwrap();
+                }
+            }
+        }
     }
 }
