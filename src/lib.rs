5aa9fc8f1ba8c474008e5eed1e6d583aa5e70a3f
wcampbell <wcampbell1995@gmail.com>
2024-10-05 10:00:28 -0400

    Update to latest deku

--- src/lib.rs
+++ src/lib.rs
@@ -84,7 +84,7 @@ const UBI_VOL_NAME_MAX: usize = 127;
 
 #[derive(Debug, DekuRead, DekuWrite, PartialEq, Eq, Hash)]
 #[deku(ctx = "endian: deku::ctx::Endian", endian = "endian")]
-#[deku(type = "u8")]
+#[deku(id_type = "u8")]
 pub enum VolType {
     /// Volume can be resized
     Dynamic = 1,
